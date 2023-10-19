c4af0b8879a8ab6d2a3e07a13722921c7cb389f9
Melioo <chosse.martiny01@gmail.com>
2023-10-19 20:38:19 +0200

    feat(SWI-14): Brand model creation
    Created Brand model for cars

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    # api_cars ADMIN stuff
+    "api_cars.apps.ApiCarsConfig",
 ]
 
 MIDDLEWARE = [
