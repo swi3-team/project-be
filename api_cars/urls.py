afea14933e82aac564b557f6dffacc5faeb44123
Mel <chosse.martiny01@gmail.com>
2023-12-06 13:41:50 +0100

    fix(SWI-27): Fix of fix
    Fixed the views to correctly allow multiple methods.

--- api_cars/urls.py
+++ api_cars/urls.py
@@ -4,16 +4,11 @@ from .views import *
 
 urlpatterns = [
     # ------------------ BRANDS ------------------
-    path("brands", BrandListView.as_view(), name="brands_list"),
-    path("brands", BrandAddView.as_view(), name="brand_add"),
+    path("brands", BrandAPIView.as_view(), name="brands_list_add"),
     # ------------------ OWNERS ------------------
-    path("owners", OwnerListView.as_view(), name="owners_list"),
+    path("owners", OwnerAPIView.as_view(), name="owners_list_add"),
     path("owners/<int:id>", OwnerDetailView.as_view(), name="owner_detail"),
-    path("owners", OwnerAddView.as_view(), name="owner_add"),
     # ------------------ CARS ------------------
-    path("", CarListView.as_view(), name="cars_list"),
-    path("<int:id>", CarDetailView.as_view(), name="car_detail"),
-    path("", CarAddView.as_view(), name="car_add"),
-    path("<int:id>", CarEditView.as_view(), name="car_edit"),
-    path("<int:id>", CarDeleteView.as_view(), name="car_delete"),
+    path("", CarAPIView.as_view(), name="cars_list_add"),
+    path("<int:id>", CarDetailAPIView.as_view(), name="car_detail_update_delete"),
 ]
